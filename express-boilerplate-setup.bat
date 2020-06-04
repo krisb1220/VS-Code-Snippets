cca9974e6658f1cfdbe139aab33f2f6e630aa58a
Kris Baillargeon <krisb1220@gmail.com>
2020-06-04 00:32:32 -0400

    Added express setup batch script
    This script makes it so there are little setup steps required to use !ebp
    
    It DOESN'T the following:
    -Install the required packages
    -Updates package.json to reflect added packages
    
    It DOES do the following:
    -Makes directories
    -Makes required files
    -Runs npm init

---
+++ express-boilerplate-setup.bat
@@ -0,0 +1,27 @@
+echo > server.js
+echo > .env
+
+mkdir auth 
+cd auth 
+echo > auth.js
+cd ../
+
+mkdir database
+cd database 
+mkdir schemas
+cd schemas 
+echo > schemas.js
+cd ../
+cd ../
+
+mkdir routes
+cd routes 
+echo > routes.js 
+cd ../
+cd ../
+
+mkdir public
+
+npm init
+
+
